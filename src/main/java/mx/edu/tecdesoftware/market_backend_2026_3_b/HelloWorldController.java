aca287d419b9152a8dc43d78ae9975db496b87ba
Diego Ramirez Magaña <dramirezmagana@gmail.com>
2026-05-12 11:07:14 -0600

    Test: adding HelloWorldController to test Spring

--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/HelloWorldController.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/HelloWorldController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+
+
     @GetMapping("/saludar")
     public String helloWorld() {
         return "Hello world";
